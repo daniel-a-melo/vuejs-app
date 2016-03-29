bd1c54385a44a280fe3ac9ea5911cf4ea873ef2f
Daniel Melo <daniel-araujo.melo@serpro.gov.br>
2016-03-29 19:07:58 -0300

    Added non vue component; bumped package versions

---
+++ src/components/hello/hello-component.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+Vue.component('hello-component', {
+  template : '<div>Hello!</div>'
+});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Counter from './components/counter.vue';
 import Reverse from './components/reverse.vue';
+import './components/hello/hello-component';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ router.map({
   },
   '/counter': {
     component: Counter
+  },
+  '/hello' : {
+    component : {
+      template: '<div><hello-component /></div>'
+    }
   }
 });
 
